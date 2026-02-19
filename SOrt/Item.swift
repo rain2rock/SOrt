328107c52dadb6407fb2b7e992ab401119f25776
Chivilev Mikhail <chivilev.mikhail@gmail.com>
2026-02-19 13:31:59 +0300

    Initial Commit

---
+++ SOrt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SOrt
+//
+//  Created by Mikhail Chivi on 19.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SOrt/SOrtApp.swift
@@ -0,0 +1,32 @@
+//
+//  SOrtApp.swift
+//  SOrt
+//
+//  Created by Mikhail Chivi on 19.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SOrtApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
